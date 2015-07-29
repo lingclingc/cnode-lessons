fed594f5d2ee76dcdc5597837ba677a16d44a8f8
lingc <linguochi@gmail.com>
2015-07-29 17:59:18 +0800

    提交lesson4  安装了新的jsformat插件，完善代码风格

--- lesson3/app.js
+++ lesson3/app.js
@@ -9,27 +9,27 @@ var baseUrl = 'https://www.cnodejs.org';
 
 var app = express();
 
-app.get('/',function (req,res,next) {
+app.get('/', function(req, res, next) {
 	superagent.get('https://cnodejs.org/')
-	.end(function (err,sres) {
-		if (err) {
-			return next(err);
-		}
-		var $ = cheerio.load(sres.text);
-		var items = [];
-		$('#topic_list .topic_title').each(function (idx,element){
-			var $element = $(element);
-			var $authorParent = $element.parentsUntil('.cell');
-			items.push({
-				title:$element.attr('title'),
-				href:url.resolve(baseUrl,$element.attr('href')),
-				author:$authorParent.children('.user_avatar').children('img').attr('title'),
+		.end(function(err, sres) {
+			if (err) {
+				return next(err);
+			}
+			var $ = cheerio.load(sres.text);
+			var items = [];
+			$('#topic_list .topic_title').each(function(idx, element) {
+				var $element = $(element);
+				var $authorParent = $element.parentsUntil('.cell');
+				items.push({
+					title: $element.attr('title'),
+					href: url.resolve(baseUrl, $element.attr('href')),
+					author: $authorParent.children('.user_avatar').children('img').attr('title'),
 				});
-		});		
-		res.send(items);
-	});
+			});
+			res.send(items);
+		});
 });
 
-app.listen(3000,function(){
+app.listen(3000, function() {
 	console.log('app js listening at port 3000');
 })
