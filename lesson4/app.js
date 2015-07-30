339dbd8a84618bbf35770396e9efdb8c6cb12da1
lingc <linguochi@gmail.com>
2015-07-30 23:52:34 +0800

    nothing

--- lesson4/app.js
+++ lesson4/app.js
@@ -23,14 +23,25 @@ superagent.get(cnodeUrl)
 		var ep = new eventproxy();
 
 		ep.after('topic_html', topicUrls.length, function(topics) {
+			//这时候topics的结构是一系列的 [topicUrl, res.text] 对构成的数组
 			topics = topics.map(function(topicPair) {
 				var topicUrl = topicPair[0];
 				var topicHtml = topicPair[1];
 				var $ = cheerio.load(topicHtml);
+				var 	author1HomeUrl = '',
+					author1Name = '';
+					author1Credits = 0;
+				//author1HomeUrl = 	url.resolve(cnodeUrl, $('.author_content .user_avatar').eq(0).attr('href'));
+				author1HomeUrl = 	$('.author_content .user_avatar').eq(0).attr('href');
+				if (typeof author1HomeUrl !== 'undefined') {
+					author1HomeUrl = url.resolve(cnodeUrl, author1HomeUrl);
+					author1Name = (cheerio.load(author1HomeUrl)).html();
+				};
 				return ({
 					title: $('.topic_full_title').text().trim(),
 					href: topicUrl,
 					comment1: $('.reply_content').eq(0).text().trim(),
+					author1: author1Name,
 				});
 			});
 
